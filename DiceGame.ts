6db341673f466577524505d9496ba86a881d985a
dcpneto <dcp_neto@live.com>
2018-03-02 09:36:01 -0300

    Update DiceGame.ts

--- DiceGame.ts
+++ DiceGame.ts
@@ -3,7 +3,11 @@ export{}
 let pt1 : number = 0
 let pt2 : number = 0
 
-function rolarDado(){
+function rolarDadoJogadorUm(){
+    return Math.floor(Math.random()*6+1);
+}
+
+function rolarDadoJogadorDois(){
     return Math.floor(Math.random()*6+1);
 }
 
@@ -12,14 +16,16 @@ function jogarDados(d1: number, d2:number){
     let rodadas : number = 0;
 
     while (rodadas < 5){
-        pt1 += rolarDado();
-        pt2 += rolarDado();
+        pt1 += rolarDadoJogadorUm();
+        pt2 += rolarDadoJogadorDois();
         
-        //pts = 0;
         rodadas++;
+        console.log("O score do jogador Um foi de: " + pt1);
+        console.log("O score do jogador Dois foi de: " + pt2);
+        
     }
 
-    jogarDados(6,6);
-    console.log("O score do jogador Um foi de: " + pt1);        
+    jogarDados(6,6);       
 }
-console.log("O score do jogador Um foi de: " + pt1);
+
+jogarDados(6,6);
